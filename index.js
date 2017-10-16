84c6107b4e99f0fbccdd3f78fa21725b14ce5f5a
Nick Papoutsis <izybit@gmail.com>
2017-10-16 13:01:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing() {
+function nowServing(katzDeli) {
   if (katzDeli.length > 0) {
     return "Currently serving " + katzDeli.shift() + ".";
   } else {

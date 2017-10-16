74eaae0c9b12ab407fd0639de3efad6941d552f5
Nick Papoutsis <izybit@gmail.com>
2017-10-16 12:46:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing() {
-  if (katzDeli.length > 0) {
+  if (katzDeliLine.length > 0) {
     return "Currently serving " + katzDeli.shift() + ".";
   } else {
     return "There is nobody waiting to be served!"
